6c6cf1e59c12007cda9fa4fa08644f3e78b6a694
Newton Wamiti <144038101+Wamitinewton@users.noreply.github.com>
2026-03-28 19:45:30 +0300

    add endpoint to retrieve list of students

--- src/main/java/com/newton/hellodevops/controller/HelloController.java
+++ src/main/java/com/newton/hellodevops/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -24,6 +25,37 @@ public class HelloController {
                 "status", "Running"
         );
     }
+
+    @GetMapping("/students")
+    public List<Map<String, Object>> getStudents() {
+        return List.of(
+                Map.of(
+                        "id" , 1,
+                        "name", "Joe Doe",
+                        "email", "devops@example.com",
+                        "course", "Computer Science"
+                ),
+                Map.of(
+                        "id" , 1,
+                        "name", "Joe Doe",
+                        "email", "devops@example.com",
+                        "course", "Computer Science"
+                ),
+                Map.of(
+                        "id" , 1,
+                        "name", "Joe Doe",
+                        "email", "devops@example.com",
+                        "course", "Computer Science"
+                ),
+                Map.of(
+                        "id" , 1,
+                        "name", "Joe Doe",
+                        "email", "devops@example.com",
+                        "course", "Computer Science"
+                )
+        );
+    }
+
 
     @GetMapping("/health")
     public Map<String, String> health() {
